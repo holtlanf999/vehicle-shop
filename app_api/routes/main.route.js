d5e7d5eda5ce6b5f2c71f784ceb94e3b429fafce
Lanford Gabriel Murillo <holtlanf999@gmail.com>
2016-03-13 01:05:34 -0600

    get all by category routes created

--- app_api/routes/main.route.js
+++ app_api/routes/main.route.js
@@ -2,13 +2,65 @@ var request = require('request').defaults({json: true}),
     config = require('../config.js'),
     async = require('async');
 
-
 module.exports = function (app) {
   
   /*Main Routes*/
   var userServer = config.userServer,
       carServer = config.carServer,
       bikeServer = config.bikeServer;
+
+  /*Create*/
+
+  /*Create User*/
+  app.post(config.mainRoute + config.userRoute, function (req, res) {
+    request.post({url: config.userServer + config.userRoute, form: req.body}, function (err, response, body) {
+      if (err) {
+        res.end('Failed to create new user');
+        return
+      }
+      if (!err && res.statusCode === 200 ) {
+        res.end('User Created Succesfully');
+        return
+      } else {
+        res.end('statusCode');
+        return
+      }
+    });
+  });
+
+  /*Create car*/
+  app.post(config.mainRoute + config.carRoute, function (req, res) {
+    request.post({url: config.carServer + config.carRoute, form: req.body}, function (err, response, body) {
+      if (err) {
+        res.end('Failed to create new car');
+        return
+      }
+      if (!err && res.statusCode === 200 ) {
+        res.end('car Created Succesfully');
+        return
+      } else {
+        res.end('statusCode');
+        return
+      }
+    });
+  });
+  
+  /*Create Bike*/
+  app.post(config.mainRoute + config.bikeRoute, function (req, res) {
+    request.post({url: config.bikeServer + config.bikeRoute, form: req.body}, function (err, response, body) {
+      if (err) {
+        res.end('Failed to create new bike');
+        return
+      }
+      if (!err && res.statusCode === 200 ) {
+        res.end('bike Created Succesfully');
+        return
+      } else {
+        res.end('statusCode');
+        return
+      }
+    });
+  });
 
   /*Read*/
 
@@ -73,6 +125,7 @@ module.exports = function (app) {
     });
   });
 
+  /*Get cars only*/
   app.get(config.mainRoute + config.carRoute, function (req, res) {
     request({ uri: config.carServer + config.carRoute}, function (error, response, body) {
       if (error) {
@@ -82,6 +135,7 @@ module.exports = function (app) {
     });
   });
 
+  /*Get bikes only*/
   app.get(config.mainRoute + config.bikeRoute, function (req, res) {
     request({ uri: config.bikeServer + config.bikeRoute}, function (error, response, body) {
       if (error) {
@@ -90,5 +144,4 @@ module.exports = function (app) {
       res.send(response.body.data);
     });
   });
-
 }
